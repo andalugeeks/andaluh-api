c9d67af6f19f74adf8eb5839e06eee87a872adf5
J. Félix Ontañón <felixonta@gmail.com>
2018-11-22 20:51:47 +0100

    Added CORS support

--- api.py
+++ api.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from controllers import epaController
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+cors = CORS(app, resources=r'/epa/*')
 api = Api(app)
 
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
